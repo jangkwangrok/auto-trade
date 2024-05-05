459b7cc4d5ca4c9cb3527f3d3f1f5c1e93c47efd
jangkwangrok <168962511+jangkwangrok@users.noreply.github.com>
2024-05-05 18:31:35 +0900

    Update test.py

--- upbit/test.py
+++ upbit/test.py
@@ -2,8 +2,8 @@ import pyupbit
 
 import pyupbit
 
-access = "2bpxnn6gmgNH6Tw5jtNzlbQ5ykPgbh4OLSUeWs4e"          # 본인 값으로 변경
-secret = "2cMIRaeVw8L5xRI1Q4WZgUkh63dMNbrfu5GyOcVr"          # 본인 값으로 변경
+access = "your-access"          # 본인 값으로 변경
+secret = "your-secret"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
